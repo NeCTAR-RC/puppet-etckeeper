3104686207fb7632e3c57154bc8ea3393ac362fd
Marcus Furlong <furlongm@gmail.com>
2013-02-22 13:50:50 +1100

    etckeeper initial config
    Change-Id: I29e77d17b555b310b8ecf2c0f4e7dfd195974399

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+class etckeeper {
+
+  $etckeeper_pkgs = ['etckeeper', 'git']
+
+  package { $etckeeper_pkgs:
+    ensure  => installed,
+    notify  => File['/etc/etckeeper/etckeeper.conf']
+  }
+
+  file { '/etc/etckeeper/etckeeper.conf':
+    mode      => 0644,
+    source    => 'puppet:///modules/etckeeper/etckeeper.conf',
+    subscribe => Package[$etckeeper_pkgs],
+    notify    => Exec['etckeeper-init'],
+  }
+
+  exec { etckeeper-init:
+    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+    command     => 'etckeeper init -d /etc',
+    subscribe   => File['/etc/etckeeper/etckeeper.conf'],
+    unless      => 'test -d /etc/.git',
+  }
+}
